da8d7052013cc4c717c0c2e254a27a085f1ab4a1
Nicolas Vaagen <nicolasvaagen@gmail.com>
2023-09-03 11:24:02 -0400

    changes

--- brackets.java
+++ brackets.java
@@ -2,7 +2,23 @@
 
 public class brackets {
 
+public int num-players;
+
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    DataInputStream in = new DataInputStream(System.in);
+    System.out.println("enter # players:");
+
+    num-players = Integer.parseInt(in.readln());
+    player players[numPlayer];
+
+    //initialize players
+    for(int i; i<numPlayers; i++){
+      System.out.print("name:");
+      players[i].changeName(in.readln());
+
+    }
   }
+
+
+  player players[numPlayer];
 }
